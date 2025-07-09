f141897f01c9b48f65a432ffe48176879e8550a9
Chad Harper <chadharper811@gmail.com>
2025-07-08 20:09:05 -0500

    finished new search by id and search by name practice

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const isBooleanInputValid = function (input) {
 
 const isStartYearValid = function (input) {
   let numValue = Number(input);
-  if (!Number.isInteger(numValue) || numValue < 1990 || numValue >2023) {
+  if (!Number.isInteger(numValue) || numValue < 1990 || numValue > 2023) {
     return false;
   }
   return true;
@@ -76,6 +76,7 @@ function listEmployees() {
   employees.forEach(e => {
     logEmployee(e);
     prompt('Press enter to continue...');
+    console.log('');
   });
 
   console.log('Emplyee list completed');
@@ -85,6 +86,7 @@ function addEmployee() {
   console.log('Add Employee -----------------------');
   console.log('');
   let employee = {};
+
   employee.firstName = getInput('First Name: ', isStringInputValid);
   employee.lastName = getInput('Last Name: ', isStringInputValid);
   let startDateYear = getInput('Employee Start Year (1990-2023): ', isStartYearValid);
@@ -92,6 +94,42 @@ function addEmployee() {
   let startDateDay = getInput('Employee Start Date Day (1-31): ', isStartDayValid);
   employee.startDate = new Date(startDateYear, startDateMonth - 1, startDateDay);
   employee.isActive = getInput('Is Employee Active (yes or no): ', isBooleanInputValid, i => (i === "yes"));
+
+  // Output Employee JSON
+  const json = JSON.stringify(employee, null, 2);
+  console.log(`Employee: ${json}`);
+}
+
+// Search for employee by ID
+function searchById() {
+  const id = getInput('Employe ID: ', null, Number);
+  const result = employees.find(e => e.id === id);
+  if (result) {
+    console.log('');
+    logEmployee(result);
+  } else {
+    console.log('No results...');
+  }
+}
+
+// Search for amployees by name
+function searchByName() {
+  const firstNameSearch = getInput('First Name: ').toLowerCase();
+  const LastNameSearch = getInput('Last Name: ').toLowerCase();
+  const results = employees.filter(e => {
+    if (firstNameSearch && !e.firstName.toLowerCase().includes(firstNameSearch)) {
+      return false;
+    }
+    if (LastNameSearch && !e.lastName.toLowerCase().includes(LastNameSearch)) {
+      return false;
+    }
+    return true;
+  });
+  results.forEach((e, idx) => {
+    console.log('');
+    console.log(`Search Result ${idx + 1} -------------------------------`);
+    logEmployee(e);
+  })
 }
 
 // Application execution -------------------------------------------------------
@@ -109,7 +147,15 @@ switch (command) {
   case 'add':
     addEmployee();
     break;
+  
+  case 'search-by-id':
+    searchById();
+    break;
 
+  case 'search-by-name':
+    searchByName();
+    break;
+    
   default:
     console.log(`Unsupposrted command. Exiting...`);
     process.exit(1);
