eb05886714b8bbfe8a91f572b09d489319736ac5
Chad Harper <chadharper811@gmail.com>
2025-07-26 15:20:00 -0500

    testing global await

--- callbacks/async-await.js
+++ callbacks/async-await.js
@@ -1,8 +1,26 @@
 // Import Node Filesystem Module (the promise version)
 import fs from "node:fs/promises";
 
-async function loadData() {
-    try {
+// async function loadData() {
+//     try {
+//         const data = await fs.readFile('./data.json', 'utf8');
+//         console.log('File Read 1');
+//         await fs.readFile('./data.json', 'utf8');
+//         console.log('File Read 2');
+//         await fs.readFile('./data.json', 'utf8');
+//         console.log('File Read 3');
+//         const dataObj = JSON.parse(data);
+//         console.log(dataObj);
+//         console.log("Complete");
+//     } catch (err) {
+//         console.log("Could not load and parse file");
+//         throw err;
+//     }
+// }
+
+// loadData().then(() => console.log("Promise completed"));
+
+try {
         const data = await fs.readFile('./data.json', 'utf8');
         console.log('File Read 1');
         await fs.readFile('./data.json', 'utf8');
@@ -16,6 +34,3 @@ async function loadData() {
         console.log("Could not load and parse file");
         throw err;
     }
-}
-
-loadData().then(() => console.log("Promise completed"));
