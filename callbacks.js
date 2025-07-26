890a6604307a2f0fe1393c858550536795f0b059
Chad Harper <chadharper811@gmail.com>
2025-07-26 14:19:05 -0500

    added new data from project practice files and started new file to practice callback functions

---
+++ callbacks.js
@@ -0,0 +1,12 @@
+// Import Node Filesystem Module (the callback version)
+import fs from "node:fs";
+
+fs.readFile('./data1.json', 'utf8', (err, data) => {
+    if(err) {
+        console.log('Error reading the file');
+        throw err;
+    }
+    const dataObj = JSON.parse(data);
+    console.log(dataObj);
+    console.log("Complete");
+});
