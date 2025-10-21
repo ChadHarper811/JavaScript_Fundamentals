5dd9aace084425ff3f3ee42219a84f8e17e310a8
Chad Harper <chadharper811@gmail.com>
2025-10-21 07:46:35 -0500

    moved code for getting slaray and currency converstion to own file and rescoped variables into currency.js

---
+++ Multifile/currency.js
@@ -0,0 +1,25 @@
+// Currency Data -------------------------------------------------------------------
+
+export const getCurrencyConversionData = async () => {
+  const headers = new Headers();
+  headers.append("access_key", "b4cf58735baebc157e7d24a2b8bd3cc1");
+  const options = {
+    method: "GET",
+    redirect: 'follow',
+    headers
+  };
+  const response = await fetch("https://api.exchangeratesapi.io/v1/latest?access_key=b4cf58735baebc157e7d24a2b8bd3cc1", options);
+  if(!response.ok) {
+    throw new Error("Cannot fetch curreny data.");
+  }
+  return await response.json();
+}
+
+export const getSalary = (amountUSD, currency) => {
+  const amount = (currency === "USD") ? amountUSD : amountUSD * currencyData.rates[currency];
+  const formatter = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency
+  });
+  return formatter.format(amount);
+}
--- Multifile/index.js
+++ Multifile/index.js
@@ -6,32 +6,6 @@
 let employees = [];
 let currencyData;
 
-// Currency data ---------------------------------------------------------
-
-const getCurrencyConversionData = async () => {
-  const headers = new Headers();
-  headers.append("apikey", "<INSERT YOUR API KEY>");
-  const options = {
-    method: "GET",
-    redirect: 'follow',
-    headers
-  };
-  const response = await fetch(`https://api.apilayer.com/exchangerates_data/latest?base=USD`, options);
-  if(!response.ok) {
-    throw new Error("Cannot fetch currency data.");
-  }
-  currencyData = await response.json();
-}
-
-const getSalary = (amountUSD, currency) => {
-  const amount = (currency === "USD") ? amountUSD : amountUSD * currencyData.rates[currency];
-  const formatter = Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: currency
-  });
-  return formatter.format(amount);
-}
-
 import createPrompt from 'prompt-sync';
 let prompt = createPrompt();
 
