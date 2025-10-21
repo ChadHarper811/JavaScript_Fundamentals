d26357275c3f3b6db1c9ec0196e00f917869b77c
Chad Harper <chadharper811@gmail.com>
2025-10-21 07:41:17 -0500

    changed code in data file to be exported and not have variables out of scope

--- Multifile/data.js
+++ Multifile/data.js
@@ -2,18 +2,18 @@ import fs from 'node:fs/promises';
 
 // Loading and writing data to the filesystem ----------------------------
 
-const loadData = async () => {
+export const loadData = async () => {
   console.log("Loading employees.....");
   try {
     const fileData = await fs.readFile('./data.json');
-    employees = JSON.parse(fileData);
+    return JSON.parse(fileData);
   } catch (err) {
     console.error("Cannot load in employees");
     throw err;
   }
 }
 
-const writeData = async () => {
+export const writeData = async (employees) => {
   console.log("Writing employees.....");
   try {
     await fs.writeFile('./data.json', JSON.stringify(employees, null, 2));
